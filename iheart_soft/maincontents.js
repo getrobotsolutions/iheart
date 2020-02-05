381c8b9233a211d5110ceadc65ee0af410769fd1
Robot Solution <manage.ars1@gmail.com>
2020-02-05 17:12:07 -0500

    init

---
+++ iheart_soft/maincontents.js
@@ -0,0 +1,244 @@
+﻿var speak = new Array( );
+
+
+        speak[0] = "Take a look at our robot at some of our previous trade shows";
+        speak[1] = "hear testimonials from trade show clients";
+        speak[2] = "spin the wheel for your chance to win great prizes";
+        speak[3] = "Read a welcome message from our dean.";
+        speak[4] = "Find out what classes you need for your major.";
+        speak[5] = "";
+        speak[6] = "Say Cheese! ";
+        speak[7] = "Try your luck! spin the slot machine once and try to win a great prize";
+        speak[8] = "Pick a song and let’s boogie!";
+        speak[9] = "Smile! You are about to become a robot.";
+
+//window.external.ChangeLanguage("en-us");
+function FC_ContentsCall(strContentsName, strLanguage)
+{
+   // alert(strContentsName);
+    
+   
+    switch (strContentsName)
+    {
+        case "Welcome":
+            PlaySpeech("welcome, please press any button to begin");
+            break;
+        case "Home":
+           location.href = "../../main.htm";
+            break;
+        case "HFV":
+            //PlaySpeech(speak[0]);
+            location.href = "Contents/HFV/index.html";
+            break;
+        case "Touchstone":
+            //PlaySpeech(speak[1]);
+            location.href = "Contents/Touchstone/index.html";
+            break;
+        case "Van-Vilet":
+            //PlaySpeech(speak[2]);
+            location.href = "Contents/Van-Vilet/index.html";
+            break;
+        case "Spectrum":
+            //PlaySpeech(speak[3]);
+            location.href = "Contents/Spectrum/index.html";
+            break;
+        case "Color-Reflections":
+            //PlaySpeech(speak[4]);
+            location.href = "Contents/Color-Reflections/index.html";
+            break;
+        case "Group-Imaging":
+            //PlaySpeech(speak[5]);
+            location.href = "Contents/Group-Imaging/index.html";
+            break;
+        
+
+        case "Selfie":
+            //PlaySpeech(speak[6]);
+            location.href = "../../Contents/Selfie/index.html";
+            break;
+        case "TakePhotos":
+            //PlaySpeech(speak[]);
+            location.href = "Contents/TakePhotos/index.html";
+            break;
+        case "Slot-machine":
+            PlaySpeech(speak[7]);
+            location.href = "Contents/SlotMachine/index.html";
+            break;
+        case "Dance":
+            //PlaySpeech(speak[8]);
+            location.href = "Contents/Dance/index.html";
+            break;
+        case "Avatar":
+            //PlaySpeech(speak[9]);
+            location.href = "../../Contents/RobotAvatar/index.htm";
+            break;
+        case "Config":
+            location.href = "Config/Config.htm";
+            break;
+        case "maincontents":
+            location.href = "maincontents.htm";
+            break;
+        default:
+            break;
+    } // end switch(strContentsName)
+} // end FC_ContentsCall
+
+
+
+function OnUserApproached()
+{
+    //PlaySpeech("Hello, welcome to the event. Please press a button on my screen to begin.");
+}
+
+
+
+function ShowPopup(){
+
+// get the screen height and width
+    var maskHeight = $(document).height();
+    var maskWidth = $(window).width();
+    // calculate the values for center alignment
+    var dialogTop =  '30%';//(maskHeight/3) - ($('#dialog-box').height());
+    var dialogLeft = (maskWidth/2) - ($('#dialog-box').width()/2);
+    // assign values to the overlay and dialog box
+    $('#dialog-overlay').css({height:maskHeight, width:maskWidth}).show();
+    $('#dialog-box').css({top:dialogTop, left:dialogLeft}).show();
+    document.getElementById('dialog-box').innerHTML = '<a href="#" class="button">Close</a><div class="dialog-content"><div id="dialog-message"><img width="800" src="assets/contact.png"/></div></div>';
+}
+function ShowImgPopup(src){
+
+// get the screen height and width
+    var maskHeight = $(document).height();
+    var maskWidth = $(window).width();
+    // calculate the values for center alignment
+    var dialogTop =  '30%';//(maskHeight/3) - ($('#dialog-box').height());
+    var dialogLeft = (maskWidth/2) - ($('#dialog-box').width()/2);
+    // assign values to the overlay and dialog box
+    $('#dialog-overlay').css({height:maskHeight, width:maskWidth}).show();
+    $('#dialog-box').css({top:dialogTop, left:dialogLeft}).show();
+    document.getElementById('dialog-box').innerHTML = '<a href="#" class="button">Close</a><div class="dialog-content"><div id="dialog-message"><img width="800" src="'+src+'"/></div></div>';
+}
+
+$(document).ready(function(){
+
+    $('a.btn-ok, #dialog-overlay, #dialog-box').click(function () {
+        $('#dialog-overlay, #dialog-box').hide();
+        return false;
+    });
+})
+
+
+ /*setTimeout(function () {
+    ShowTime();
+    console.log("Time Showed");
+
+    var city = "Fairfield, CT";
+    var searchtext = "select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='f'";
+    var queryURL = "https://query.yahooapis.com/v1/public/yql?q="+ searchtext + "&format=json";
+
+   $.getJSON(queryURL, function (data) {
+
+        var results = data.query.results;
+        var firstResult = results.channel.item.condition;
+        console.log(firstResult);
+
+
+        var location = 'Unknown'; // not returned in response
+        var temp = firstResult.temp;
+        var text = firstResult.text;
+        var image =  firstResult.code;
+        var loc = 'https://s.yimg.com/zz/combo?a/i/us/we/52/'+image+'.gif' ;
+
+        // $('#temp').append('The temperature is <strong>' + temp + '</strong><sup>°F</sup> Forecast calls for '+text);
+
+        $('#condition').append(text);
+        $('#temp').append(temp+ '</strong><sup>°F</sup>');
+
+        $('#image-zoom').attr("src",loc);
+
+        console.log("Weather Showed");
+    });
+
+}, 2000);*/
+
+
+
+function ShowTime()
+{
+    var dt = new Date();
+    // formatAMPM(dt);
+    document.getElementById("content_air") .innerHTML = formatAMPM(dt) ;
+    document.getElementById("content_date") .innerHTML = formatDate(dt);
+
+}
+function formatAMPM(date) {
+
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+
+
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+
+    var dayName = days[date.getDay()];
+
+    //dayName = date.toString().split(' ')[0];
+    hours = hours <10? '0' +hours : hours;
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = dayName + ' ' + hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+    //alert(strTime);
+}
+
+function formatDate(date){
+
+    var m_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var month = m_names[date.getMonth()];
+    var day = date.getDate();
+    day = getGetOrdinal(day);
+
+    var output = (month<10 ? '0' : '') + month + ' ' +(day<10 ? '0' : '') + day+', '+ date.getFullYear() ;
+    return output;
+}
+
+function getGetOrdinal(n) {
+    var s=["th","st","nd","rd"],
+        v=n%100;
+    return n+'<sup>'+(s[(v-20)%10]||s[v]||s[0])+'</sup>';
+}
+
+function OnJoystickControlled(strPara){
+    var btn_info = strPara.split(',')[4];
+
+
+    if(btn_info[0] == '1'){
+            window.external.ChangeLanguage("en-us");
+            SetVolume(1);
+            window.external.PlaySpeech("Hey there, have you seen the Océ Colorado--the hottest roll in town?");//I'm here to assist you, press any button on my screen to begin");
+          
+    }
+
+    if(btn_info[1] == '1'){
+        window.external.ChangeLanguage("en-us");
+        SetVolume(1);
+        window.external.PlaySpeech("Hey would you like to stop babysitting your printer? Then you need to see the Océ Colorado.");
+        
+    }
+    if(btn_info[2] == '1'){
+        window.external.ChangeLanguage("en-us");
+            SetVolume(1);
+            window.external.PlaySpeech("Hi there, my name is Hannah, why don't you take a break and watch one of my videos. Just push the button on my screen!");
+    }
+    if(btn_info[3] == '1'){
+        window.external.ChangeLanguage("en-us");
+        SetVolume(1);
+        window.external.PlaySpeech("Hey why don't you take a break, come on in and see our next presentation from Canon See Impossible.");
+        
+    }
+    if(btn_info[4] == '1'){
+        FC_ContentsCall('Home');
+    }
+}
